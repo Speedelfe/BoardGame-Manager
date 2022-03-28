6a54b34fced3ef3f25519039ecafaab68eb33420
Speedelfe <trautmann.n@mailbox.org>
2022-03-28 15:35:57 +0000

    Initial commit

---
+++ src/Program.fs
@@ -0,0 +1,71 @@
+namespace BoardGameManager
+
+open Elmish
+open Avalonia.FuncUI
+open Avalonia.FuncUI.DSL
+open Avalonia.FuncUI.Elmish
+open Avalonia.FuncUI.Hosts
+open Avalonia.FuncUI.Types
+open Avalonia
+open Avalonia.Controls
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Diagnostics
+open Avalonia.Layout
+
+[<AutoOpen>]
+module Blaa =
+    let init () = ()
+
+    let update msg state = state
+
+    let view state dispatch : IView =
+        DockPanel.create [
+            DockPanel.children [
+                TextBlock.create [
+                    TextBlock.text "Hello Boardgame!"
+                ]
+            ]
+        ]
+
+type MainWindow() as this =
+    inherit HostWindow()
+
+    do
+        base.Title <- "BoardGame Manager"
+        base.Width <- 800.0
+        base.Height <- 600.0
+
+        //this.VisualRoot.VisualRoot.Renderer.DrawFps <- true
+        //this.VisualRoot.VisualRoot.Renderer.DrawDirtyRects <- true
+
+#if DEBUG
+        this.AttachDevTools()
+#endif
+
+        Program.mkSimple init update view
+        |> Program.withHost this
+#if DEBUG
+        |> Program.withConsoleTrace
+#endif
+        |> Program.run
+
+type App() =
+    inherit Application()
+
+    override this.Initialize() =
+        this.Styles.Load "avares://Avalonia.Themes.Default/DefaultTheme.xaml"
+        this.Styles.Load "avares://Avalonia.Themes.Default/Accents/BaseDark.xaml"
+
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktopLifetime -> desktopLifetime.MainWindow <- MainWindow()
+        | _ -> ()
+
+module Program =
+    [<EntryPoint>]
+    let main (args: string []) =
+        AppBuilder
+            .Configure<App>()
+            .UsePlatformDetect()
+            .UseSkia()
+            .StartWithClassicDesktopLifetime(args)
