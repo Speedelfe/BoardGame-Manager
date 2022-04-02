3c999025fca0a3d0dbf4a7777111e670dbf40a50
Speedelfe <trautmann.n@mailbox.org>
2022-04-02 13:21:20 +0000

    Types verfeinert

--- src/Core/Types.fs
+++ src/Core/Types.fs
@@ -1,4 +1,53 @@
 namespace BoardGameManager
 
 module Types =
-    type Game = { name: string; playerNumbers: int }
+
+    type GameGenre =
+        | "strategy"
+        | "card"
+        | "raetsel"
+        | "wuerfel"
+        | "enginebuilder"
+        | "party"
+        | "other"
+
+    type ShelfName =
+        | "Wohnwand-Links"
+        | "Wohnwand-Rechts"
+        | "Sofa-Schrank"
+        | "Büro-Regal"
+
+    type GameJson =
+        {
+            name: string
+            playerNumbers: int
+            playTime: float
+            lastPlayed: string
+            genre: string
+            secondaryGenre: string
+            shelf: string
+            description: string
+        }
+    type Game =
+        {
+            name: string;
+            playerNumbers: int
+            playTime: float
+            lastPlayed: Option DateTime
+            genre: GameGenre
+            secondaryGenre: Opion List<GameGenre>
+            shelf: string
+            description: Option string
+        }
+
+    type GameList =
+        {
+            games: list<Game>
+        }
+
+    type Shelf =
+        {
+            name: ShelfName;
+            games: List<Game>
+        }
+
